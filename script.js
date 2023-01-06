d332ba6eb5440ae9c88ba591a11bf6341cc1c953
Dasom Heo <dasomee@outlook.com>
2023-01-06 18:17:51 -0500

    Confirm password display feedback icon and text

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
 const pwField = document.querySelector('#pwd');
 const pwConfField = document.querySelector('#pwd_cnf');
 const pwBubble = document.querySelector('div.pw-bubble')
+const pwConfBubble = document.querySelector('div.pw-cnf-bubble')
 
-//grab all requirement li's
+//grab all requirement li's for validation feedback
 const charReq = document.querySelector('li.char');
 const capReq = document.querySelector('li.cap');
 const numReq = document.querySelector('li.num');
 let passValidity = false;
 
-//confirmation icon appears after focusout
+//input confirmation icon appears only after focusout
 pwField.addEventListener('focusout', () => {
     pwField.classList.add('viewed-by-user');
 });
+pwConfField.addEventListener('input', () => {
+    if(pwField.value.length > 0){
+        pwConfField.classList.add('viewed-by-user')
+        pwConfBubble.classList.add('viewed-by-user')
+    }
+});
+
+pwConfField.addEventListener('focusout', () => {
+    if(pwField.value.length > 0){
+        pwConfField.classList.add('viewed-by-user')
+        pwConfBubble.classList.add('viewed-by-user')
+    }
+});
+
+pwConfField.addEventListener('input',() => {
+    pwConfField.classList.add('viewed-by-user')
+    pwConfBubble.classList.add('viewed-by-user')
+})
 
 pwField.addEventListener('input',() => {
     const enteredValue = pwField.value;
@@ -28,7 +47,7 @@ pwField.addEventListener('input',() => {
     }
 })
 
-//evaluate characters requirements
+//evaluate reqs
 function charCountCheck(text) {
     if (text.length >= 8) {
         charReq.classList.add('valid');
@@ -58,3 +77,20 @@ function numCheck(text) {
         numReq.classList.remove('valid');
     }
 }
+
+//password confirmation check
+document.addEventListener('input', () => {
+    const password = pwField.value;
+    const confirmation = pwConfField.value;
+    if(password == confirmation && password.length > 0) {
+        pwConfField.classList.add('valid')
+        pwConfBubble.classList.add('hide')
+    } else if (password.length == 0 && confirmation.length == 0){
+        pwConfBubble.classList.add('hide')
+    } else {
+        pwConfField.classList.remove('valid')
+        pwConfBubble.classList.remove('hide')
+    }
+})
+
+
